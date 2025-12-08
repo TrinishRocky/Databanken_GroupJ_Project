bce5ce3f4193b2cee7648905f360dbed03233eeb
TrinishRocky <trinish.nepal@student.tugraz.at>
2025-12-08 14:52:40 +0100

    Delete IngestDataGroupJ.sql

--- IngestDataGroupJ.sql
+++
@@ -1,4 +0,0 @@
-COPY Platforms FROM 'C:\postgres_import\platforms.csv' DELIMITER ',' CSV HEADER;
-COPY Users FROM 'C:\postgres_import\users.csv' DELIMITER ',' CSV HEADER;
-COPY BehavioralUsageMetrics FROM 'C:\postgres_import\behavioralusagemetrics.csv' DELIMITER ',' CSV HEADER;
-COPY MentalHealthMetrics FROM 'C:\postgres_import\mentalhealthmetrics.csv' DELIMITER ',' CSV HEADER;
